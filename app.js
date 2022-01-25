a6531ad9100d91da76b41a2e21c8154b4e2a64dd
Conor OBrien <conor@cosmonautair.local>
2022-01-24 21:42:23 -0600

    add work route

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 var listItems = [];
+let workItems = [];
 
+//home route
 app.get("/", function(req, res) {
   var today = new Date();
   var options = { weekday: 'long', month: 'long', day: 'numeric' };
@@ -20,22 +22,42 @@ app.get("/", function(req, res) {
 
 
   res.render('list', {
-    kindOfDay: day,
+    listTitle: day,
     newListItems: listItems
   });
 
 })
 
-//post route
+//post route for home
 app.post("/", function(req, res){
-  var listItem = req.body.newItem;
+  let listItem = req.body.newItem;
+  console.log(req.body);
 
-  listItems.push(listItem);
+//which array to send to
+  if (req.body.list === 'Work') {
+    workItems.push(listItem);
+    res.redirect("/work");
+  } else {
+    listItems.push(listItem);
+      res.redirect("/");
+  }
+})
 
-  res.redirect("/");
+//work route
+app.get("/work", function(req, res) {
+  let work = "Work List";
+
+  res.render('list', {
+    listTitle: work,
+    newListItems: workItems
+  });
 })
 
 
+//about page
+app.get("/about", function(req,res){
+  res.render("about");
+})
 
 
 app.listen(port, function() {
